c672f85871ab8ba451d0518252ce8183a5ad3363
songheekang <kshs24820@gmail.com>
2026-06-18 19:25:34 +0900

    Revert "fix: Input props를 React.InputHTMLAttributes로 확장 (#74)"
    This reverts commit 739800bc2698e47546e22029d767a10b55b8e586.

--- apps/web/src/components/common/Input.tsx
+++ apps/web/src/components/common/Input.tsx
@@ -3,16 +3,23 @@
 import { forwardRef } from "react";
 import { twMerge } from "tailwind-merge";
 
-interface InputProps extends React.InputHTMLAttributes<HTMLInputElement> {
+interface InputProps {
   placeholder: string;
+  type?: string;
+  value?: string;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  className?: string;
 }
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
-  ({ className, ...props }, ref) => {
+  ({ placeholder, type = "text", value, onChange, className }, ref) => {
     return (
       <input
         ref={ref}
-        {...props}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
         className={twMerge("bg-white border border-[#e2e2e2] rounded-[12px] h-[56px] w-full px-[21px] text-[14px] text-[#3f2a24] placeholder:text-[rgba(107,103,99,0.5)] outline-none focus:border-[#8edfd2] transition-colors", className)}
       />
     );
